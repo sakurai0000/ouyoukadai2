ae0183c69fb6770821638265f73cc24458a77d3b
sakurai0000 <107847812+sakurai0000@users.noreply.github.com>
2022-07-29 09:53:53 +0000

    Initial Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,17 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50}
 
-  
-  
+
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no-image.jpg')
@@ -21,4 +26,17 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width,height]).processed
   end
+
+  def follow(user_id)
+  relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+  relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+  followings.include?(user)
+  end
+
 end
